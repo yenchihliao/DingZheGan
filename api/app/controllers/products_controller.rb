0e0ec1ee2792d496381fe40c88d1a5c7459b5dfb
JCly-rikiu <jk123456789jp@gmail.com>
2016-04-13 15:55:04 +0800

    Fix bugs caused by the BAD api

--- api/app/controllers/products_controller.rb
+++ api/app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show]
+  before_action :set_product, only: []
 
   # GET /products
   # GET /products.json
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    render json: @product
+    render json: create_update_product(params[:ProductSN].to_i)
   end
 
   # POST /products
@@ -40,16 +40,24 @@ class ProductsController < ApplicationController
       end
     end
 
-    def create_update_product(id, vendorID = 0, vendorName = "")
-      hash = get_product(id)["Product"][0]
+    def create_update_product(id, vendorName = '')
+      result = get_product(id)
+
+      unless result['ErrorCode'] == 0 && !result['Product'].empty?
+        return { :status => 1, :data => result['ErrorMsg'] }
+      end
+
+      hash = result['Product'][0]
 
       if Product.exists?(ProductSN: id)
         p = Product.find(id)
-        p.update(:ProductSN => id, :ProductVendor => vendorID, :ProductTitle => hash["ProductTitle"], :ProductNo => hash["ProductNo"], :SellPrice => hash["SellPrice"], :SellPriceCNY => hash["SellPriceCNY"], :ProductQuantity => hash["ProductQuantity"], :ProductIntroduction => hash["ProductIntroduction"], :StyleTitleA => hash["StyleTitleA"], :StyleTitleB => hash["StyleTitleB"], :LargeIcon => hash["LargeIcon"], :SmallIcon => hash["SmallIcon"], :ProductPhoto1 => hash["ProductPhoto1"], :ProductPhoto2 => hash["ProductPhoto2"], :ProductPhoto3 => hash["ProductPhoto3"], :ProductPhoto4 => hash["ProductPhoto4"], :ProductPhoto5 => hash["ProductPhoto5"], :ProductPhoto6 => hash["ProductPhoto6"], :ProductPhoto7 => hash["ProductPhoto7"], :ProductPhoto8 => hash["ProductPhoto8"])
+        p.update(:ProductSN => id, :ProductVendor => hash['ProductVendor'], :ProductTitle => hash['ProductTitle'], :ProductNo => hash['ProductNo'], :SellPrice => hash['SellPrice'], :SellPriceCNY => hash['SellPriceCNY'], :ProductQuantity => hash['ProductQuantity'], :ProductIntroduction => hash['ProductIntroduction'], :StyleTitleA => hash['StyleTitleA'], :StyleTitleB => hash['StyleTitleB'], :LargeIcon => hash['LargeIcon'], :SmallIcon => hash['SmallIcon'], :ProductPhoto1 => hash['ProductPhoto1'], :ProductPhoto2 => hash['ProductPhoto2'], :ProductPhoto3 => hash['ProductPhoto3'], :ProductPhoto4 => hash['ProductPhoto4'], :ProductPhoto5 => hash['ProductPhoto5'], :ProductPhoto6 => hash['ProductPhoto6'], :ProductPhoto7 => hash['ProductPhoto7'], :ProductPhoto8 => hash['ProductPhoto8'])
       else
-        create_update_vendor(vendorID, vendorName)
-        Product.create(:ProductSN => id, :ProductVendor => vendorID, :ProductTitle => hash["ProductTitle"], :ProductNo => hash["ProductNo"], :SellPrice => hash["SellPrice"], :SellPriceCNY => hash["SellPriceCNY"], :ProductQuantity => hash["ProductQuantity"], :ProductIntroduction => hash["ProductIntroduction"], :StyleTitleA => hash["StyleTitleA"], :StyleTitleB => hash["StyleTitleB"], :LargeIcon => hash["LargeIcon"], :SmallIcon => hash["SmallIcon"], :ProductPhoto1 => hash["ProductPhoto1"], :ProductPhoto2 => hash["ProductPhoto2"], :ProductPhoto3 => hash["ProductPhoto3"], :ProductPhoto4 => hash["ProductPhoto4"], :ProductPhoto5 => hash["ProductPhoto5"], :ProductPhoto6 => hash["ProductPhoto6"], :ProductPhoto7 => hash["ProductPhoto7"], :ProductPhoto8 => hash["ProductPhoto8"])
+        create_update_vendor(hash['ProductVendor'], vendorName)
+        Product.create(:ProductSN => id, :ProductVendor => hash['ProductVendor'], :ProductTitle => hash['ProductTitle'], :ProductNo => hash['ProductNo'], :SellPrice => hash['SellPrice'], :SellPriceCNY => hash['SellPriceCNY'], :ProductQuantity => hash['ProductQuantity'], :ProductIntroduction => hash['ProductIntroduction'], :StyleTitleA => hash['StyleTitleA'], :StyleTitleB => hash['StyleTitleB'], :LargeIcon => hash['LargeIcon'], :SmallIcon => hash['SmallIcon'], :ProductPhoto1 => hash['ProductPhoto1'], :ProductPhoto2 => hash['ProductPhoto2'], :ProductPhoto3 => hash['ProductPhoto3'], :ProductPhoto4 => hash['ProductPhoto4'], :ProductPhoto5 => hash['ProductPhoto5'], :ProductPhoto6 => hash['ProductPhoto6'], :ProductPhoto7 => hash['ProductPhoto7'], :ProductPhoto8 => hash['ProductPhoto8'])
       end
+
+      { :status => 0, :data => hash }
     end
 
     def parse_products
@@ -60,7 +68,7 @@ class ProductsController < ApplicationController
         vendorTitle = columns[1]
         productSN = columns[2].to_i
 
-        create_update_product(productSN, vendorSN, vendorTitle)
+        create_update_product(productSN, vendorTitle)
       end
     end
 end
